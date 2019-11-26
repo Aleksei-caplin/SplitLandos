90435f1a4b7987812172c16cf8698e24d34ec4ec
Alex <temp125@br.ru>
2019-11-26 10:52:06 +0700

    развернул версию на рабочем компе

---
+++ backend/settings/prod_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'split_landos',
+        'USER': 'acmeuser',
+        'PASSWORD': '12121232',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
